078f36cc2f836d5147a1626ab99552b10ad485e0
JefG67 <jefguilpain@gmail.com>
2024-03-21 22:30:25 +0100

    modi et ajout des controller

--- index.php
+++ index.php
@@ -2,13 +2,20 @@
 session_start();
 
 use Controller\CinemaController;
-use Controller\FilmController; 
+use Controller\FilmController;
+use Controller\GenreController; 
+use Controller\RoleController;
+use Controller\PersonneController;
 
 spl_autoload_register(function ($class_name) {
     include $class_name . '.php';
 });    
     
 $ctlrCinema = new CinemaController();
+$ctrlFilm = new FilmController();
+$ctrlGenre = new GenreController();
+$ctrlRole = new RoleController();
+$ctrlPersonne = new PersonneController();
   
 
 $id = (isset($_GET["id"])) ? $_GET["id"] : null;
@@ -24,9 +31,11 @@ if (isset($_GET["action"])) {
         case "detailFilm": $ctlrCinema->detailFilm($id); break;
         
 
-        // case "ajoutGenre" :$ctlrCinema->ajoutGenre(); break;
-        case "supprimerGenre" : $ctlrCinema->supprimerGenre($id); break;
-        // case "addFilm": $ctlrCinema->ajoutFilm(); break;
+        case "supprimerFilm" :$ctlrFilm->supprimerFilm($id); break;
+        case "supprimerGenre" : $ctlrGenre->supprimerGenre($id); break;
+        case "supprimerRole" : $ctrlRole->supprimerRole($id); break;
+        case "supprimerActeur" : $ctrPersonne->supprimerActeur($id); break;
+        
     }
 } else {
     $ctlrCinema->accueil();
